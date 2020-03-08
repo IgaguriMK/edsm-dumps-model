5068aa6ae89f709bfad3ee4de878692e722e1512
Igaguri <igagurimk@gmail.com>
2020-03-08 14:07:29 +0900

    change: Normal read-based ParallelDecoder

--- src/array_decoder/parallel.rs
+++ src/array_decoder/parallel.rs
@@ -1,33 +1,29 @@
-#![allow(unused)]
-
+use std::collections::BTreeMap;
 use std::env;
-use std::io::BufRead;
-use std::path::Path;
-use std::sync::mpsc::{sync_channel, Receiver, SyncSender, TrySendError};
-use std::thread::{Builder, JoinHandle};
+use std::fs::File;
+use std::io::{self, BufRead, Read};
+use std::mem::{drop, swap};
+use std::path::{Path, PathBuf};
+use std::thread::Builder;
 use std::vec::IntoIter;
 
 use anyhow::{Context, Error};
-use filebuffer::FileBuffer;
+use crossbeam_channel::{bounded, Receiver, Sender};
 use serde_json::from_str;
 
 use super::Progress;
 use crate::model::RootEntry;
 
 const INPUT_CHUNK_SIZE: usize = 8 * 1024 * 1024;
-const MAX_INPUT_CHUNK_SIZE: usize = 12 * 1024 * 1024;
-
-const INPUT_CHANNEL_MASTER_BUFFER: usize = 1;
-const INPUT_CHANNEL_SUB_BUFFER: usize = 1;
-const OUTPUT_CHANNEL_MASTER_BUFFER: usize = 16;
-const OUTPUT_CHANNEL_SUB_BUFFER: usize = 16;
-
+const INPUT_BYTES_CHANNEL_BUF: usize = 1024;
+const INPUT_LINE_BUFFER_INITIAL_SIZE: usize = 1024;
+const PARSED_CHANNEL_BUF: usize = 256;
+const RESULT_CHANNEL_BUF: usize = 256;
 const WORKER_MULT: usize = 2;
 
 pub struct ParallelDecoder<D> {
-    handles: Vec<JoinHandle<()>>,
-    reading: Option<IntoIter<D>>,
-    recv: Receiver<Option<Vec<D>>>,
+    reading: IntoIter<D>,
+    recv: Receiver<Result<Vec<D>, Error>>,
 }
 
 impl<D: 'static + Send + RootEntry> ParallelDecoder<D> {
@@ -35,236 +31,294 @@ impl<D: 'static + Send + RootEntry> ParallelDecoder<D> {
         path: impl AsRef<Path>,
         progress: impl 'static + Send + Progress,
     ) -> Result<ParallelDecoder<D>, Error> {
-        let bytes = FileBuffer::open(path).context("failed open file")?.leak();
+        let path = path.as_ref().to_owned();
 
-        let worker_cnt = get_worker_cnt();
+        let (input_send, input_recv) = bounded(INPUT_BYTES_CHANNEL_BUF);
+        let (parsed_send, parsed_recv) = bounded(PARSED_CHANNEL_BUF);
+        let (result_send, result_recv) = bounded(RESULT_CHANNEL_BUF);
 
-        let mut sends = Vec::with_capacity(worker_cnt);
-        let mut recvs = Vec::with_capacity(worker_cnt);
-        let mut handles = Vec::with_capacity(worker_cnt + 3);
+        Builder::new()
+            .name("input reader".to_owned())
+            .spawn(move || {
+                read(path, input_send, progress);
+            })
+            .context("failed spawn input reader")?;
 
-        let (bytes_recv, h) = spawn_splitter(bytes, progress);
-        handles.push(h);
+        for i in 0..get_worker_cnt() {
+            let r = input_recv.clone();
+            let s = parsed_send.clone();
 
-        for i in 0..worker_cnt {
-            let (bytes_send, bytes_recv) = sync_channel(INPUT_CHANNEL_SUB_BUFFER);
-            let (chunk_send, chunk_recv) = sync_channel(OUTPUT_CHANNEL_SUB_BUFFER);
-
-            let h = spawn_decoder::<D>(i, bytes_recv, chunk_send);
-            sends.push(bytes_send);
-            recvs.push(chunk_recv);
-            handles.push(h);
+            Builder::new()
+                .name(format!("parser({})", i))
+                .spawn(move || {
+                    parse(r, s);
+                })
+                .with_context(|| format!("failed spawn parser({})", i))?;
         }
+        drop(input_recv);
+        drop(parsed_send);
 
-        let h = spawn_send_mux(bytes_recv, sends);
-        handles.push(h);
-
-        let (send, recv) = sync_channel(OUTPUT_CHANNEL_MASTER_BUFFER);
-        let h = spawn_recv_mux(send, recvs);
+        Builder::new()
+            .name("collector".to_owned())
+            .spawn(move || {
+                collect(parsed_recv, result_send);
+            })
+            .context("failed spawn collector")?;
 
         Ok(ParallelDecoder {
-            handles,
-            reading: None,
-            recv,
+            reading: Vec::new().into_iter(),
+            recv: result_recv,
         })
     }
 
     pub fn read_entry(&mut self) -> Result<Option<D>, Error> {
         loop {
-            if let Some(ref mut iter) = self.reading {
-                if let Some(x) = iter.next() {
-                    return Ok(Some(x));
-                } else {
-                    self.reading = None;
+            if let Some(v) = self.reading.next() {
+                return Ok(Some(v));
+            }
+
+            match self.recv.recv() {
+                Ok(Ok(vs)) => {
+                    self.reading = vs.into_iter();
                 }
-            } else {
-                match self.recv.recv().unwrap() {
-                    Some(chunk) => self.reading = Some(chunk.into_iter()),
-                    None => {
-                        for h in self.handles.drain(..) {
-                            h.join().unwrap();
-                        }
-                        return Ok(None);
-                    }
+                Ok(Err(e)) => {
+                    return Err(e);
+                }
+                Err(_) => {
+                    return Ok(None);
                 }
             }
         }
     }
 }
 
-fn spawn_splitter(
-    bytes: &'static [u8],
+fn read(
+    path: PathBuf,
+    send: Sender<(usize, Result<Vec<u8>, Error>)>,
     mut progress: impl 'static + Send + Progress,
-) -> (Receiver<Option<&'static [u8]>>, JoinHandle<()>) {
-    let (send, recv) = sync_channel(INPUT_CHANNEL_MASTER_BUFFER);
+) {
+    let f = match File::open(&path).context("failed to open input file") {
+        Ok(v) => v,
+        Err(e) => {
+            send.send((0, Err(e))).expect("failed to send input value");
+            return;
+        }
+    };
+    let mut chunk_reader = ChunkReader::new(f, INPUT_CHUNK_SIZE);
 
-    let handle = Builder::new()
-        .name("splitter".to_owned())
-        .spawn(move || {
-            let mut bytes = bytes;
+    for idx in 0usize.. {
+        match chunk_reader
+            .read_chunk()
+            .context("failed to read input chunk")
+        {
+            Ok(Some(bs)) => {
+                progress.inc(bs.len());
 
-            if bytes.starts_with(b"[\n") {
-                bytes = bytes.split_at(2).1;
-                progress.inc(2);
+                send.send((idx, Ok(bs)))
+                    .expect("failed to send input value");
             }
-
-            let reset_eta_at = (4 * 1024 * 1024 * 1024) / INPUT_CHUNK_SIZE;
-            for i in 0.. {
-                if bytes.len() < MAX_INPUT_CHUNK_SIZE {
-                    break;
-                }
-                if i % reset_eta_at == 0 {
-                    progress.reset_eta();
-                }
-
-                let pos = find_newline(bytes, INPUT_CHUNK_SIZE);
-                if pos + 1 < bytes.len() {
-                    let (bs, bss) = bytes.split_at(pos + 1);
-                    progress.inc(bs.len());
-                    send.send(Some(bs)).unwrap();
-                    bytes = bss;
-                }
+            Ok(None) => {
+                break;
             }
-
-            if bytes.ends_with(b"]") {
-                bytes = bytes.split_at(bytes.len() - 1).0;
-                progress.inc(1);
+            Err(e) => {
+                send.send((idx, Err(e))).expect("failed to send read error");
+                break;
             }
-            progress.inc(bytes.len());
-            send.send(Some(bytes)).unwrap();
-            send.send(None).unwrap();
-        })
-        .unwrap();
-    (recv, handle)
+        }
+    }
 }
 
-fn spawn_decoder<D: 'static + Send + RootEntry>(
-    idx: usize,
-    bytes_recv: Receiver<Option<&'static [u8]>>,
-    chunk_send: SyncSender<Option<Vec<D>>>,
-) -> JoinHandle<()> {
-    Builder::new()
-        .name(format!("decoder[{}]", idx))
-        .spawn(move || {
-            if let Err(e) = decoder(bytes_recv, chunk_send) {
-                eprintln!("\n\n{:?}\n\n", e);
-                panic!("decode failed")
+fn parse<D: 'static + Send + RootEntry>(
+    recv: Receiver<(usize, Result<Vec<u8>, Error>)>,
+    send: Sender<(usize, Result<Vec<D>, Error>)>,
+) {
+    let mut chunk_parser = ChunkParser::new();
+
+    while let Ok((idx, r)) = recv.recv() {
+        match r {
+            Ok(bs) => {
+                match chunk_parser
+                    .parse_chunk(bs.as_slice())
+                    .context("failed to parse input chunk")
+                {
+                    Ok(v) => {
+                        send.send((idx, Ok(v)))
+                            .expect("failed to send parsed value");
+                    }
+                    Err(e) => {
+                        send.send((idx, Err(e)))
+                            .expect("failed to send parse error");
+                        return;
+                    }
+                }
             }
-        })
-        .unwrap()
+            Err(e) => {
+                send.send((idx, Err(e)))
+                    .expect("failed to pass error in parser");
+                return;
+            }
+        }
+    }
 }
 
-fn decoder<D: 'static + Send + RootEntry>(
-    recv: Receiver<Option<&'static [u8]>>,
-    send: SyncSender<Option<Vec<D>>>,
-) -> Result<(), Error> {
-    let mut line = String::new();
+#[derive(Debug)]
+struct ChunkParser {
+    buf: String,
+    max_values_len: usize,
+}
 
-    while let Some(mut bytes) = recv.recv().unwrap() {
-        let mut chunk = Vec::new();
+impl ChunkParser {
+    fn new() -> ChunkParser {
+        ChunkParser {
+            buf: String::with_capacity(INPUT_LINE_BUFFER_INITIAL_SIZE),
+            max_values_len: 0,
+        }
+    }
+
+    fn parse_chunk<D: 'static + Send + RootEntry>(
+        &mut self,
+        mut bs: &[u8],
+    ) -> Result<Vec<D>, Error> {
+        let mut values = Vec::<D>::with_capacity(self.max_values_len);
+        let orig_bs = bs.to_owned();
+
         loop {
-            line.truncate(0);
-            let n = bytes.read_line(&mut line)?;
+            self.buf.clear();
+            let n = bs.read_line(&mut self.buf)?;
             if n == 0 {
                 break;
             }
 
-            let s = line
-                .trim_start_matches("    ")
-                .trim_end_matches('\n')
-                .trim_end_matches(',');
+            let s = self.buf.trim();
+            if s == "[" {
+                continue;
+            }
+            if s == "]" {
+                break;
+            }
+            let s = s.trim_end_matches(',');
+
             if s.is_empty() {
                 continue;
             }
-            let s = D::pre_filter(s);
 
-            let v = from_str(s.as_ref()).context(format!("failed parse entry with: {}", s))?;
-            chunk.push(v);
+            let v = from_str(s)
+                .with_context(|| format!("failed parse line({:?}): \"{}\"", orig_bs, self.buf))?;
+            values.push(v);
         }
-        send.send(Some(chunk)).unwrap();
+        if values.len() > self.max_values_len {
+            self.max_values_len = values.len();
+        }
+        Ok(values)
     }
-    send.send(None).unwrap();
-    Ok(())
 }
 
-fn spawn_send_mux<T: 'static + Send>(
-    recv: Receiver<Option<T>>,
-    sends: Vec<SyncSender<Option<T>>>,
-) -> JoinHandle<()> {
-    Builder::new()
-        .name("send_mux".to_owned())
-        .spawn(move || {
-            while let Some(x) = recv.recv().unwrap() {
-                let mut sending = Some(x);
-                'send: loop {
-                    for s in &sends {
-                        match s.try_send(sending) {
-                            Ok(_) => break 'send,
-                            Err(TrySendError::Full(s)) => sending = s,
-                            Err(TrySendError::Disconnected(_)) => {
-                                panic!("unexpected disconnect in send_mux")
-                            }
-                        }
-                    }
+fn collect<T: Send>(recv: Receiver<(usize, Result<T, Error>)>, send: Sender<Result<T, Error>>) {
+    let mut holding = BTreeMap::<usize, Result<T, Error>>::new();
+    let mut next = 0;
+
+    loop {
+        match recv.recv() {
+            Ok((idx, r)) => {
+                holding.insert(idx, r);
+            }
+            Err(_) => {
+                // Sender Closed
+                return;
+            }
+        }
+
+        // Send following values
+        while let Some(r) = holding.remove(&next) {
+            match r {
+                Ok(v) => {
+                    send.send(Ok(v)).expect("failed to send collected value");
+                    next += 1;
                 }
-            }
-
-            for s in &sends {
-                s.send(None).unwrap();
-            }
-        })
-        .unwrap()
-}
-
-fn spawn_recv_mux<T: 'static + Send>(
-    send: SyncSender<Option<T>>,
-    recvs: Vec<Receiver<Option<T>>>,
-) -> JoinHandle<()> {
-    Builder::new()
-        .name("recv_mux".to_owned())
-        .spawn(move || {
-            let mut recvs = recvs;
-            loop {
-                if recvs.is_empty() {
-                    send.send(None).unwrap();
+                Err(e) => {
+                    send.send(Err(e)).expect("failed to send collected error");
                     return;
                 }
-
-                let mut end_idx = Option::<usize>::None;
-                for (i, r) in recvs.iter().enumerate() {
-                    match r.try_recv() {
-                        Ok(Some(x)) => {
-                            send.send(Some(x)).unwrap();
-                            break;
-                        }
-                        Ok(None) => {
-                            end_idx = Some(i);
-                            break;
-                        }
-                        Err(_) => {}
-                    }
-                }
-                if let Some(i) = end_idx {
-                    recvs.remove(i);
-                }
             }
-        })
-        .unwrap()
+        }
+    }
 }
 
 fn get_worker_cnt() -> usize {
-    if let Ok(s) = env::var("THREADS") {
+    if let Ok(s) = env::var("EDM_THREADS") {
         s.parse().unwrap()
     } else {
         WORKER_MULT * num_cpus::get()
     }
 }
 
-fn find_newline(buf: &[u8], start: usize) -> usize {
-    for (i, b) in buf.iter().enumerate() {
-        if *b == b'\n' {
-            return i;
+struct ChunkReader<R: Read> {
+    chunk_size: usize,
+    inner: R,
+    left_buf: Vec<u8>,
+    left_size: usize,
+}
+
+impl<R: Read> ChunkReader<R> {
+    fn new(inner: R, chunk_size: usize) -> ChunkReader<R> {
+        ChunkReader {
+            chunk_size,
+            inner,
+            left_buf: alloc_vec(chunk_size),
+            left_size: 0,
         }
     }
-    buf.len()
+
+    fn read_chunk(&mut self) -> Result<Option<Vec<u8>>, io::Error> {
+        let mut buf = alloc_vec(self.chunk_size);
+        swap(&mut buf, &mut self.left_buf);
+
+        let mut total_read = self.left_size;
+
+        // Read
+        while total_read < self.chunk_size {
+            let n = self.inner.read(&mut buf[total_read..])?;
+            total_read += n;
+
+            if n == 0 {
+                break;
+            }
+        }
+
+        // return `None` if empty
+        if total_read == 0 {
+            return Ok(None);
+        }
+
+        // return final chunk
+        if total_read < self.chunk_size {
+            buf.truncate(total_read);
+            self.left_size = 0;
+            return Ok(Some(buf));
+        }
+
+        // find newline
+        let mut split_pos = total_read - 1;
+        while split_pos > 0 {
+            if buf[split_pos] == b'\n' {
+                let left = &buf[split_pos + 1..];
+                self.left_size = left.len();
+                self.left_buf[0..left.len()].copy_from_slice(left);
+
+                buf.truncate(split_pos + 1);
+                return Ok(Some(buf));
+            }
+
+            split_pos -= 1;
+        }
+
+        // no newline in buffer
+        self.left_size = 0;
+        Ok(Some(buf))
+    }
+}
+
+fn alloc_vec(len: usize) -> Vec<u8> {
+    let layout = std::alloc::Layout::from_size_align(len, 1).expect("failed create layout");
+    unsafe { Vec::from_raw_parts(std::alloc::alloc_zeroed(layout) as *mut _, len, len) }
 }
