cc0ea2b318c456253fcddd46debeb38d733b0e77
Igaguri <igagurimk@gmail.com>
2020-04-16 08:58:24 +0900

    feat: operators for Coords

--- src/model/system.rs
+++ src/model/system.rs
@@ -1,3 +1,5 @@
+use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub, SubAssign};
+
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 
@@ -40,13 +42,110 @@ impl RootEntry for SystemWithoutCoordinates {}
 
 // Field Type
 
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, Copy, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 #[serde(deny_unknown_fields)]
 pub struct Coords {
     pub x: f32,
     pub y: f32,
     pub z: f32,
+}
+
+impl Coords {
+    pub fn abs(self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2) + self.z.powi(2)).sqrt()
+    }
+
+    pub fn dist(self, other: Coords) -> f32 {
+        (self - other).abs()
+    }
+}
+
+impl Add for Coords {
+    type Output = Self;
+    fn add(self, other: Self) -> Self {
+        Coords {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+        }
+    }
+}
+
+impl AddAssign for Coords {
+    fn add_assign(&mut self, other: Self) {
+        self.x += other.x;
+        self.y += other.y;
+        self.z += other.z;
+    }
+}
+
+impl Div<f32> for Coords {
+    type Output = Self;
+    fn div(self, other: f32) -> Self {
+        Coords {
+            x: self.x / other,
+            y: self.y / other,
+            z: self.z / other,
+        }
+    }
+}
+
+impl DivAssign<f32> for Coords {
+    fn div_assign(&mut self, other: f32) {
+        self.x /= other;
+        self.y /= other;
+        self.z /= other;
+    }
+}
+
+impl Mul<f32> for Coords {
+    type Output = Self;
+    fn mul(self, other: f32) -> Self {
+        Coords {
+            x: self.x * other,
+            y: self.y * other,
+            z: self.z * other,
+        }
+    }
+}
+
+impl MulAssign<f32> for Coords {
+    fn mul_assign(&mut self, other: f32) {
+        self.x *= other;
+        self.y *= other;
+        self.z *= other;
+    }
+}
+
+impl Neg for Coords {
+    type Output = Self;
+    fn neg(self) -> Self {
+        Coords {
+            x: -self.x,
+            y: -self.y,
+            z: -self.z,
+        }
+    }
+}
+
+impl Sub for Coords {
+    type Output = Self;
+    fn sub(self, other: Self) -> Self {
+        Coords {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+        }
+    }
+}
+
+impl SubAssign for Coords {
+    fn sub_assign(&mut self, other: Self) {
+        self.x -= other.x;
+        self.y -= other.y;
+        self.z -= other.z;
+    }
 }
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
